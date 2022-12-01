4cc1d07b73520bdf3d411f80164a84172253d95a
Seth A Burleson <Sbrevolution5@aim.com>
2022-12-01 10:34:43 -0500

    initail setup

--- Advent2022/Program.cs
+++ Advent2022/Program.cs
@@ -1,3 +1,31 @@
 ﻿
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+while (true)
+{
+    Console.WriteLine("Please enter the day you wish to solve");
+    string day = Console.ReadLine();
+    long dayNum = 0;
+    if (!Int64.TryParse(day, out dayNum))
+    {
+        Console.WriteLine("Number Not detected, input 0 to exit.");
+    }
+    else
+    {
+        if(dayNum == 0)
+        {
+            break;
+        }
+        else
+        {
+            switch (dayNum)
+            {
+                case 1:
+                    dayNum = 1;
+                    break;
+                default:
+                    Console.WriteLine("Number is either over 25 or puzzle not complete for that day");
+                    break;
+            }
+        }
+    }
+}
