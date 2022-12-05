632076656462153e7a39bbbf63628a55eec3ce16
Seth A Burleson <Sbrevolution5@aim.com>
2022-12-05 09:46:27 -0500

    p1 ugly

---
+++ Advent2022/Models/Sack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2022.Models
+{
+    public class Sack
+    {
+        public string Input { get; set; }
+        public List<string> Halves { get; set; }
+        public char Duplicate { get; set; }
+        public int Priority { get; set; }
+    }
+}
--- Advent2022/Services/RucksackSorter.cs
+++ Advent2022/Services/RucksackSorter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Advent2022.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,8 +12,45 @@ namespace Advent2022.Services
         public int GetDuplicatePriority(string input)
         {
             List<string> sackstrings = ParseInput(input);
+            List<Sack> sacks = ProcessSacks(sackstrings);
+            return sacks.Select(s=>s.Priority).Sum();
+        }
 
-            return 0;
+        private List<Sack> ProcessSacks(List<string> sackstrings)
+        {
+            var res = new List<Sack>();
+            foreach (var sack in sackstrings)
+            {
+                var x = new Sack();
+                x.Input = sack;
+                res.Add(x);
+            }
+            foreach (var sack in res)
+            {
+                SplitInput(sack);
+                FindDuplicate(sack);
+                SetPriority(sack);
+            }
+            throw new NotImplementedException();
+        }
+
+        private void FindDuplicate(Sack sack)
+        {
+            sack.Duplicate = 'a';
+        }
+
+        private void SetPriority(Sack sack)
+        {
+            sack.Priority = sack.Priority - 50;
+        }
+
+        private void SplitInput(Sack sack)
+        {
+            var halfwaypt = sack.Input.Length / 2;
+            var h1 = sack.Input.Substring(0, halfwaypt);
+            var h2 = sack.Input.Substring(halfwaypt, halfwaypt - 1);
+            sack.Halves.Add(h1);
+            sack.Halves.Add(h2);
         }
 
         private List<string> ParseInput(string input)
