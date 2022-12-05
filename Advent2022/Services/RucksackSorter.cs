82b0913c04edac22e2019f2a40f4b8666c38195e
Seth A Burleson <Sbrevolution5@aim.com>
2022-12-05 09:34:15 -0500

    setup and testsetup

---
+++ Advent2022/Services/RucksackSorter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2022.Services
+{
+    public class RucksackSorter
+    {
+        public int GetDuplicatePriority(string input)
+        {
+            List<string> sackstrings = ParseInput(input);
+
+            return 0;
+        }
+
+        private List<string> ParseInput(string input)
+        {
+            var res = new List<string>();
+            var lines = input.Split("\r\n");
+            foreach (var line in lines)
+            {
+                res.Add(line);
+            }
+            return res;
+        }
+    }
+}
---
+++ Advent2022Tests/Services/RucksackSorterTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Advent2022.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace Advent2022.Services.Tests
+{
+    [TestClass()]
+    public class RucksackSorterTests
+    {
+        private readonly string _input = @"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+        [TestMethod()]
+        public void GetDuplicatePriorityTest()
+        {
+            var sorter = new RucksackSorter();
+            var result = sorter.GetDuplicatePriority(_input);
+            result.Should().Be(157);
+        }
+    }
+}
